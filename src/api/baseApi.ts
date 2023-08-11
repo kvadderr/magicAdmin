9b475a29f5d6212e7cb6db4b32943c8de00de8d6
VMironuxa <116167552+VMironuxa@users.noreply.github.com>
2023-08-11 17:49:52 +0300

    Update baseApi.ts

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,5 +1,18 @@
-import { baseUrl } from '../helpers/baseUrl';
+import { baseApi } from './settings';
 
-export const getIncome = () => {
-  return fetch(`${baseUrl}/statistic/today`).then((response) => response.json());
+export const getIncomeToday = async () => {
+  const response = await baseApi.get<any>('/statisctic/today');
+  return response.data;
+};
+export const getIncome30Days = async () => {
+  const response = await baseApi.get<any>('/statisctic/last30');
+  return response.data;
+};
+export const getIcomeMonth = async () => {
+  const response = await baseApi.get<any>('/statisctic/month');
+  return response.data;
+};
+export const getIcomeAllTime = async () => {
+  const response = await baseApi.get<any>('/statisctic/allTime');
+  return response.data;
 };
