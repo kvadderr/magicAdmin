c26b08c2db1a8efe26b7a9a595ac69f491660f68
VMironuxa <116167552+VMironuxa@users.noreply.github.com>
2023-08-25 18:55:59 +0300

    0 visible

--- src/components/statistic/Base/Amount.tsx
+++ src/components/statistic/Base/Amount.tsx
@@ -10,10 +10,11 @@ interface AmountProps {
     | undefined;
 }
 const Amount = ({ text, data }: AmountProps) => {
+  if (data === undefined) return <div>...</div>;
   return (
     <div className="container-base">
-      {data?.profit ? (
-        <div className="content-base">{text + ': ' + data?.profit + '₽'}</div>
+      {data?.profit !== undefined ? (
+        <div className="content-base">{text + ': ' + (data?.profit != null ? data.profit : 0) + '₽'}</div>
       ) : (
         <div className="content-base">
           {text + ': ' + (data?.avgCount ? data?.avgCount : data?.avarageDeposit + '₽')}
