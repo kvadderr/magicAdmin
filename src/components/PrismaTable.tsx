a8ff7a1e16a708231429ea6e46143a8fc0668f64
VMironuxa <116167552+VMironuxa@users.noreply.github.com>
2023-10-04 21:21:10 +0300

    style

--- src/components/PrismaTable.tsx
+++ src/components/PrismaTable.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import styled from 'styled-components';
 import { PrismaTable } from '@paljs/admin/PrismaTable';
 
 const Table: React.FC<{ model: string }> = ({ model }) => {
   const router = useRouter();
-  return <PrismaTable model={model} push={router.push} query={router.query} />;
-};
 
-export default Table;
+  // then pass here
+  const PrismaTableStyled = styled(PrismaTable)`
+    & td {
+      max-width: 12rem;
+    }
+  `;
+
+  return <PrismaTableStyled model={model} push={router.push} query={router.query} />;
+};
